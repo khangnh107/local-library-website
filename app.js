dc4ff2cf891c07da50fa19a18ac8cc7342135a42
Khang Huu Nguyen <nhk2005.personal@gmail.com>
2023-12-16 17:01:20 -0500

    Add route handlers to project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var coolRouter = require("./routes/cool");
+const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/users/cool", coolRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
