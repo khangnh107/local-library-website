09f160496dcb124c83bd1d0e1757eb1c29110edc
Khang Huu Nguyen <nhk2005.personal@gmail.com>
2023-12-18 20:54:57 -0500

    Fix bug

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,8 +2,8 @@ const Book = require("../models/book");
 const Author = require("../models/author");
 const Genre = require("../models/genre");
 const BookInstance = require("../models/bookinstance");
-
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details of books, book instances, authors and genre counts (in parallel)
